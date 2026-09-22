ab849b3190cad6ecc24f039bb4c2918286f1f2c4
darklight9911 <muniaedu911@gmail.com>
2026-09-23 01:22:07 +0600

    Add solution for problem 3

---
+++ prob3.py
@@ -0,0 +1,20 @@
+meaning = {
+    "table":["a piece of furniture","list of facts & figures"],
+    "cat": "a small animal"
+}
+print(meaning)
+
+subjects = ["python","java","C++","python","javascript","java","python","java","C++","C"]
+classroom = len(set(subjects))
+print(classroom)
+
+result = {}
+result.update({"Physics": 90,"Chemistry":95,"English":90})
+print(result)
+
+a = 9
+b = 9.0
+sett = set()
+sett.add(a)
+sett.add(str(b))
+print(sett)
